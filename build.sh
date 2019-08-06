18309758dab2dc27521a7886e5919461c890ba8c
Billy Fisher <billyjfisher@gmail.com>
2019-08-05 21:21:19 -0700

    ./build.sh & ./run.sh successful.

---
+++ build.sh
@@ -0,0 +1,1 @@
+javac -d out/helloworld src/helloworld/com/javamodularity/helloworld/HelloWorld.java src/helloworld/module-info.java
---
+++ run.sh
@@ -0,0 +1,1 @@
+java --module-path helloworld.jar --module helloworld
